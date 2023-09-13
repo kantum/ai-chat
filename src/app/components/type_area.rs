46046c6d144b64b4a1a5a27aa581ca93b975900c
Kantum <42kantum@gmail.com>
2023-09-13 21:39:13 +0200

    feat: french is better

--- src/app/components/type_area.rs
+++ src/app/components/type_area.rs
@@ -14,8 +14,8 @@ pub fn TypeArea(
             send.dispatch(input.value());
             input.set_value("");
         } class="flex items-center p-4 bg-slate-700">
-            <input type="text" placeholder="Enter your prompt" node_ref=input_ref class="w-full bg-slate-800 rounded-lg border border-slate-700 text-white focus:outline-none focus:ring focus:border-slate-700 px-4 py-2" />
-            <input type="submit" class="ml-2 rounded-lg bg-blue-500 px-4 py-2 text-white cursor-pointer"/>
+            <input type="text" placeholder="Posez votre question" node_ref=input_ref class="w-full bg-slate-800 rounded-lg border border-slate-700 text-white focus:outline-none focus:ring focus:border-slate-700 px-4 py-2" />
+            <input type="submit" class="ml-2 rounded-lg bg-blue-500 px-4 py-2 text-white cursor-pointer" value="Envoyer"/>
         </form>
     }
 }
